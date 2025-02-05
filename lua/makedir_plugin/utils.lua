af9eda5206f6092dab0b02f58180a087e85c81d0
Pedro Vitor <pvmonteiro26@gmail.com>
2025-02-05 20:32:19 -0300

    Fix print message

--- lua/makedir_plugin/utils.lua
+++ lua/makedir_plugin/utils.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.make_dir(dir)
   local cmd = "mkdir -p " .. vim.fn.shellescape(dir)
   vim.fn.system(cmd)
-  print("Directory created:" .. dir)
+  print("Directory created: " .. dir)
 end
 
 return M
