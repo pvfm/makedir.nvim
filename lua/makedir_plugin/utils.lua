eec27a5aedbe2e391b65735b6c48c421c9e79750
Pedro Vitor <pvmonteiro26@gmail.com>
2025-02-05 20:31:00 -0300

    Fix bug

--- lua/makedir_plugin/utils.lua
+++ lua/makedir_plugin/utils.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.make_dir(dir)
-  local cmd = "mkdir -p" .. vim.fn.shellescape(dir)
+  local cmd = "mkdir -p " .. vim.fn.shellescape(dir)
   vim.fn.system(cmd)
   print("Directory created:" .. dir)
 end
